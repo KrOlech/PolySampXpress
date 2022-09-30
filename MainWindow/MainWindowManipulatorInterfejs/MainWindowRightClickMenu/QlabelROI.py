6550fd1540f446a417fdcacc581016a4d9d6be70
Krzysztof <Krzysztof_Olech@interia.pl>
2022-09-30 14:23:43 +0200

    Button position over right click menu Button Colors

--- MainWindow/MainWindowManipulatorInterfejs/MainWindowRightClickMenu/QlabelROI.py
+++ MainWindow/MainWindowManipulatorInterfejs/MainWindowRightClickMenu/QlabelROI.py
@@ -4,11 +4,9 @@ from PyQt5 import QtGui
 from PyQt5.QtCore import QRect, QPoint, Qt, QEvent
 from abc import abstractmethod
 from abc import ABCMeta
-from MainWindow.MainWindowManipulatorInterfejs.MainWindowRightClickMenu.MainWindowCameraGUI.CameraGUI import CameraGUI
 from MainWindowRightClickMenu.RightClickMenu import RightMenu, RightClickLabel
 from utilitis.Abstract import abstractmetod
 from ROI.ROI import ROI
-from PyQt5.QtWidgets import QWidget, QVBoxLayout
 from PyQt5.QtWidgets import QApplication
 
 
@@ -164,33 +162,3 @@ class QlabelROI(RightClickLabel):
         QApplication.setOverrideCursor(Qt.ArrowCursor)
         self.editTrybe = False
 
-
-class CameraGUIextention(CameraGUI):
-
-    def __init__(self, windowsize, *args, **kwargs) -> None:
-        super(CameraGUIextention, self).__init__(*args, **kwargs)
-
-        self.setFixedSize(windowsize)
-
-        self.widget = QWidget()
-        self.setCentralWidget(self.widget)
-
-        self.cameraView = QlabelROI(self)
-        self.cameraView.setFixedSize(windowsize)
-
-        self.layout_box = QVBoxLayout(self.widget)
-        self.layout_box.setContentsMargins(0, 0, 0, 0)
-        self.layout_box.addWidget(self.cameraView)
-
-
-if __name__ == '__main__':
-    from PyQt5.QtWidgets import QApplication
-    import sys
-
-    app = QApplication(sys.argv)
-
-    window = CameraGUIextention(app.desktop().availableGeometry().size())
-
-    window.show()
-
-    app.exec_()
