2e251008a7bbddd875694109fca830683f43d069
Krzysztof <Krzysztof_Olech@interia.pl>
2022-11-04 10:34:13 +0100

    Un achiwed legit wersion Unknown errore moved to next relies Host side Client side

--- manipulator/TCIPManipulator.py
+++ manipulator/TCIPManipulator.py
@@ -19,6 +19,21 @@ class Worker(QObject):
         self.master.conn, self.master.addr = self.master.socket.accept()
         self.finished.emit()
 
+class Worker1(QObject):
+    finished = pyqtSignal()
+
+    def __init__(self, master, *args, **kwargs):
+        super(Worker1, self).__init__(*args, **kwargs)
+        self.master = master
+
+    def run(self):
+        data = self.master.socket.recv(self.master.BUFFER_SIZE)
+        print(data.decode('utf8'))
+        if data.decode('utf8') == "ok":
+            print("end of movement")
+            self.finished.emit()
+
+
 
 class TCIPManipulator(AbstractManipulator):
     TCP_IP = "172.30.254.65"  # SOCKET.gethostbyname(SOCKET.gethostname())
@@ -61,10 +76,25 @@ class TCIPManipulator(AbstractManipulator):
             try:
                 self.conn.send(("x" + str(self.x)).encode("utf8"))
                 self.conn.send(("y" + str(self.y)).encode("utf8"))
+
+                thread = QThread()
+                worker = Worker1(self)
+
+                worker.moveToThread(thread)
+
+                thread.started.connect(worker.run)
+                worker.finished.connect(thread.quit)
+                worker.finished.connect(worker.deleteLater)
+                thread.finished.connect(thread.deleteLater)
+
+                thread.start()
+
             except AttributeError:
                 print("TCIP manipulator not yet connected")
                 self.x = 25.0
                 self.y = 25.0
+
+
 
     def validateSpeed(self, speed):
         return speed <= 10
