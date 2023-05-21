08d9a098351b0dd6e563b436342afb2dad1aad99
PC <Krzystof_olech@interia.pl>
2023-05-21 14:06:25 +0200

    new optional shortcut for ManipulatorInterfejs Action banding for master allowing usage of shortcut

--- src/Manipulator/Interfejs/ManipulatorInterfejs.py
+++ src/Manipulator/Interfejs/ManipulatorInterfejs.py
@@ -8,7 +8,13 @@ class ManipulatorInterfere(AbstractManipulatorInterfejs):
     def __init__(self, master, *args, **kwargs):
         super(ManipulatorInterfere, self).__init__(master, *args, **kwargs)
 
-        keyboard = [Qt.Key_Up, Qt.Key_Left, Qt.Key_Right, Qt.Key_Down]
+        # toDo no simple two shortcut for single action
+        # keyboard = [Qt.Key_W, Qt.Key_A, Qt.Key_D, Qt.Key_S]
+        keyboard2 = [Qt.Key_Up, Qt.Key_Left, Qt.Key_Right, Qt.Key_Down]
 
-        [a.setShortcut(k) for a, k in zip(self.actions, keyboard)]
+        # [a.setShortcut(k) for a, k in zip(self.actions, keyboard)]
+        [a.setShortcut(k) for a, k in zip(self.actions, keyboard2)]
 
+        [a.setShortcutContext(Qt.WindowShortcut) for a in self.actions]
+
+        [self.master.addAction(a) for a in self.actions]
