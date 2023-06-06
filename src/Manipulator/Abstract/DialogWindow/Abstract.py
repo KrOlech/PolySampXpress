72bfa22a081332d210dba284f39fa8dcf5aabb84
PC <Krzystof_olech@interia.pl>
2023-06-06 21:07:27 +0200

    Change from sttop to stop

--- src/Manipulator/Abstract/DialogWindow/Abstract.py
+++ src/Manipulator/Abstract/DialogWindow/Abstract.py
@@ -23,7 +23,7 @@ class AbstractDialog(QDialog, Loger):
 
     @property
     def CancelName(self):
-        return "Sttop"
+        return "Stop"
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
