64dd9bb6d3a164aeb569085873c514a63870788a
Zenbook <Krzysztof_olech@interia.pl>
2024-11-08 11:18:20 +0100

    F 117 Pull wiue in hand mode

--- src/Python/BackEnd/ROI/Creation/ClickCreate/PointerMode.py
+++ src/Python/BackEnd/ROI/Creation/ClickCreate/PointerMode.py
@@ -2,14 +2,17 @@ from Python.BackEnd.ROI.Creation.Abstract.Abstract import CreateRoiAbstract
 
 
 class PointerMode(CreateRoiAbstract):
+    pressed = False
+
     def __savePressLocation(self, e):
-        pass
+        self.pressed = True
 
     def __seveReliseLocation(self, e):
-        pass
+        self.pressed = False
 
     def __saveTemporaryLocation(self, e):
-        pass
+        if self.pressed:
+            self.mainWindow.manipulatorInterferes.center(e.x(), e.y(), self.mainWindow.zoom)
 
     def __toggleModeCleenUp(self):
-        pass
+        self.pressed = False
