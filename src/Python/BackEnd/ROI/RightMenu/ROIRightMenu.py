a2a28ac9f0a43a886734f604a29ebf303e1418ef
Zenbook <Krzysztof_olech@interia.pl>
2024-12-07 09:16:58 +0100

    F 2 54 Rename in ROI Menu

--- src/Python/BackEnd/ROI/RightMenu/ROIRightMenu.py
+++ src/Python/BackEnd/ROI/RightMenu/ROIRightMenu.py
@@ -10,7 +10,10 @@ class RoiRightMenu(QMenu):
 
         edit = self.addAction("Edit ROI")
         centerOn = self.addAction("Center On")
+        rename = self.addAction("Rename")
         delete = self.addAction("delete ROI")
         edit.triggered.connect(master.edit)
-        delete.triggered.connect(master.delete)
         centerOn.triggered.connect(master.centerOnMe)
+        rename.triggered.connect(master.rename)
+        delete.triggered.connect(master.delete)
+
