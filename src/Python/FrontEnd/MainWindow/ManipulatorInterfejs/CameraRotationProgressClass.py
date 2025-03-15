12a5a6e7698a8c27a181f14fcb3b4d6d73e42aee
x1 <Krzysztof_Olech@interia.pl>
2025-03-15 16:49:52 +0100

    B 79 2 Stop for camera rotation calculation

--- src/Python/FrontEnd/MainWindow/ManipulatorInterfejs/CameraRotationProgressClass.py
+++ src/Python/FrontEnd/MainWindow/ManipulatorInterfejs/CameraRotationProgressClass.py
@@ -3,6 +3,15 @@ from Python.Utilitis.GenericProgressClass import GenericProgressClass
 
 class CameraRotationProgressClass(GenericProgressClass):
 
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.finaliseGUISingleButtonCancel()
+
     def end(self):
         self.accept()
         self.master.showResultsRotationCalculation()
+
+    def cancelPressed(self):
+        self.master.StopTheRotationCalculation = True
+        self.accept()
